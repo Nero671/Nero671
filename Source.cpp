1469e93f6caa45aed69d5c91fa87a6559161b213
Nero671 <32450586+Nero671@users.noreply.github.com>
2017-10-09 21:00:33 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	int b;
+	cout << "¬ведите а: " << endl;
+	cin >> a;
+	cout << "¬ведите b: " << endl;
+	cin >> b;
+	cout << "a: " << a << endl;
+	cout << "b: " << b << endl << endl;
+	cout << "Sum: " << a + b << endl;
+	cout << "Odds: " << a - b << endl;
+	cout << "Composition: " << a * b << endl;
+	cout << "Division: " << a / b << " , " << a % b << endl << endl;
+}
