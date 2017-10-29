e88d063b0a57e3c3ad65cf0f46adc6ceb1e30e32
Nero671 <32450586+Nero671@users.noreply.github.com>
2017-10-29 23:36:28 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <time.h>   
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	srand(time(0)); 
+	int kot1 = rand() % 15;
+	cout << "kot1 = " << kot1 << "кг." << endl;
+	int kot2 = rand() % 15;
+	cout << "kot2 = " << kot2 << "кг" << endl;
+	if (kot1 > 5)
+		cout << "Warning! Ожирение!!" << endl;
+	if (kot2 > 5) 
+		cout << "Warning! Ожирение!!" << endl;
+}
